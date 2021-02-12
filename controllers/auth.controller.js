5feffbbc5d3e991a91d2c4dba1c519d4daffe2fa
Latortuequiparle <Turtle.Dev33@gmail.com>
2021-02-12 11:02:19 +0100

    Continuation du projet et ajout de l'update de la bio d'un profil

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,7 @@
 const UserModel = require('../models/user.model');
 
 
-
+//Creation d'un utilisateur, on a besoin de son pseudo, son email et son mot de passe. c'est tout
 module.exports.signUp = async(req, res) => {
     console.log(req.body);
     const {pseudo, email, password} = req.body;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,9 +3,13 @@ const authController = require('../controllers/auth.controller');
 const userController = require('../controllers/user.contoller');
 
 
-
+//On definit les routes a suivre et quels sont les modules a executer sur celui-ci pour recuperer (get) ou envoyer (post) des donnees
 router.post('/register', authController.signUp);
 
+
 router.get('/', userController.getAllUsers);
+router.get('/:id', userController.userInfo);
+
+router.put('/:id', userController.updateUser);
 
 module.exports = router;
