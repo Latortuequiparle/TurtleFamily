bfcbee6409a359604d6c7ee1a1524ff434538272
Latortuequiparle <Turtle.Dev33@gmail.com>
2021-02-12 12:26:44 +0100

    Ajout de la suppression d'un membre

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,7 +3,7 @@ const authController = require('../controllers/auth.controller');
 const userController = require('../controllers/user.contoller');
 
 
-//On definit les routes a suivre et quels sont les modules a executer sur celui-ci pour recuperer (get) ou envoyer (post) des donnees
+//On definit les routes a suivre et quels sont les modules a executer sur celui-ci pour recuperer (get), envoyer (post), modifier (put), supprimer (delete) des donnees
 router.post('/register', authController.signUp);
 
 
@@ -11,5 +11,7 @@ router.get('/', userController.getAllUsers);
 router.get('/:id', userController.userInfo);
 
 router.put('/:id', userController.updateUser);
+
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
